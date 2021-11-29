3b15c34bba9c8024d9987efed205fce2decd9ddd
Alexandra <alexandraavendano@github.com>
2021-11-28 23:32:15 -0800

    Proyecto esqueleto para creacion de formularios basicos

---
+++ src/registro/Encuesta.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// TODO: Implementar un formulario con los siguientes campos: nombre, estado civil, y fecha de nacimiento.  Usar el metodo POST /add del back.
+function Encuesta() {
+    return (
+        <div>
+            <h1>Registrar nueva encuesta</h1>
+        </div>
+    );
+}
+
+export default Encuesta;
---
+++ src/resultado/HistorialEncuestas.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//TODO: Implementar una tabla que muestre todos los datos de las encuestas enviadas. Usar el metodo GET /all del back.
+function HistorialEncuestas() {
+    return (
+        <div>
+            <h2>Resultado Encuestas</h2>
+        </div>
+    );
+}
+
+export default HistorialEncuestas;
